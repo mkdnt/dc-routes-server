f5f7b0f505c334ce1284fad2660f4ff3e87033fa
Mike Dent <mikedent85@gmail.com>
2020-12-31 17:37:16 -0500

    deployment stuff

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require('./config')
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const routeRouter = require('./routes/route-router')
@@ -13,7 +14,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 const app = express();
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({origin: CLIENT_ORIGIN}));
 app.use(validateBearerToken)
 app.use('/api/route', routeRouter)
 app.use(errorHandler)
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://MikeDent@localhost/dc-routes',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://MikeDent@localhost/dc-routes-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://owxrzedysensjc:63688b64d58db0e316601a9dc17296b8594e81830fa11ab8421afb3265954481@ec2-23-23-88-216.compute-1.amazonaws.com:5432/d84o6g5kmjd6q7',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://MikeDent@localhost/dc-routes-test',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dc-routes-client.vercel.app'
 };
